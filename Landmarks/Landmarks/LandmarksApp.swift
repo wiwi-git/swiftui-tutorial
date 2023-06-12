347ba443e6ad0bc7fc18c6c276d5f606965ad894
wiwi-git <duse34@hotmail.com>
2023-06-12 13:07:05 +0900

    Section 1 ~ 6

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by apple on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
